4844aa1e73b15ea4f2b84dea99b395b9fd143bd3
Lucía Schmidt <100483614@alumnos.uc3m.es>
2025-03-03 15:19:42 +0100

    few changes

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ data_path = "/export/data_ml4ds/bacteria_id/RAW_MaldiMaranon/data_cleaner_result
 # Preprocessing pipeline
 binning_step = 3
 preprocess_pipeline = SequentialPreprocessor(VarStabilizer(method="sqrt"),
-                                Smoother(halfwindow=10),
-                                BaselineCorrecter(method="SNIP", snip_n_iter=20),
-                                Trimmer(),
-                                Binner(step=binning_step),
-                                Normalizer(sum=1)
-                                )
+                                            Smoother(halfwindow=10),
+                                            BaselineCorrecter(method="SNIP", snip_n_iter=20),
+                                            Trimmer(),
+                                            Binner(step=binning_step),
+                                            Normalizer(sum=1))
 
 # Data augmentation
 data_augmenter = DataAugmenter() #TODO: Implement data augmentation
