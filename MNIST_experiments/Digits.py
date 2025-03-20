ef9a237c956879cd86ae7a87ad794c29167d0af4
Lucía Schmidt <100483614@alumnos.uc3m.es>
2025-03-20 19:05:13 +0100

    MNSIT experiments with VAEs

---
+++ MNIST_experiments/Digits.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from torch.utils.data import Dataset
+from sklearn.datasets import load_digits
+
+class Digits(Dataset):
+    """Scikit-Learn Digits dataset."""
+
+    def __init__(self, mode='train', transforms=None):
+        digits = load_digits()
+        if mode == 'train':
+            self.data = digits.data[:1000].astype(np.float32)
+        elif mode == 'val':
+            self.data = digits.data[1000:1350].astype(np.float32)
+        else:
+            self.data = digits.data[1350:].astype(np.float32)
+
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sample = self.data[idx]
+        if self.transforms:
+            sample = self.transforms(sample)
+        return sample
