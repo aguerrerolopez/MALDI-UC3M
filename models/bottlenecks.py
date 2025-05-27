f8f8a786e81020d6a4c36ba01557f4499eaaed21
Lucía Schmidt <100483614@alumnos.uc3m.es>
2025-05-27 12:57:42 +0200

    Update bottlenecks.py
    Make it felxible to change the last activation layer

--- models/bottlenecks.py
+++ models/bottlenecks.py
@@ -12,30 +12,38 @@ class MLP(nn.Module):
     Methods:
     """
 
-    def __init__(self, input_dim=2000, latent_size=256):
+    def __init__(self, input_dim=2000, hidden_size=512, latent_size=256, last = 'sigmoid'):
         super(MLP, self).__init__()
 
         self.input_dim = input_dim
         self.latent_size = latent_size
+        self.hidden_size = hidden_size
+        if last == 'sigmoid':
+            layer_activation = nn.Sigmoid() # Sigmoid activation for output layer to be between 0 and 1
+        elif last == 'relu':
+            layer_activation = nn.ReLU()
+        else:
+            raise ValueError("Invalid activation function for the last layer. Choose 'sigmoid' or 'relu'.")
+
 
         # Encoder: Fully connected layers
         self.encoder = nn.Sequential(
-            nn.Linear(self.input_dim, 1024),
+            nn.Linear(self.input_dim, self.hidden_size*2),
             nn.ReLU(),
-            nn.Linear(1024, 512),
+            nn.Linear(self.hidden_size*2, self.hidden_size),
             nn.ReLU(),
-            nn.Linear(512, self.latent_size),
+            nn.Linear(self.hidden_size, self.latent_size),
             nn.ReLU())
 
         # Decoder: Fully connected layers
         self.decoder = nn.Sequential(
             nn.ReLU(),
-            nn.Linear(self.latent_size, 512),
+            nn.Linear(self.latent_size, self.hidden_size),
             nn.ReLU(),
-            nn.Linear(512, 1024),
+            nn.Linear(self.hidden_size, self.hidden_size*2),
             nn.ReLU(),
-            nn.Linear(1024, self.input_dim),
-            nn.Sigmoid()  # Sigmoid activation for output layer to be between 0 and 1
+            nn.Linear(self.hidden_size*2, self.input_dim),
+            layer_activation
         )
 
     # def forward_encode(self, x):
